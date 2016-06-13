5757c6fd90722ca9f1e0f4641dc4a2076693c093
Carson Lam <carsonyylam@gmail.com>
2016-06-12 20:36:58 -0700

    Add reporting to Coveralls on Appveyor.

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -8,10 +8,13 @@ if %ERRORLEVEL% NEQ 0 exit 1
 python -m pip install -e .
 if %ERRORLEVEL% NEQ 0 exit 1
 
-python -m pip install pytest mock pytest-cov
+python -m pip install pytest mock pytest-cov coveralls
 if %ERRORLEVEL% NEQ 0 exit 1
 
 py.test tests --cov-report term-missing --cov pypac
+if %ERRORLEVEL% NEQ 0 exit 1
+
+coveralls
 if %ERRORLEVEL% NEQ 0 exit 1
 
 python setup.py bdist_wheel
