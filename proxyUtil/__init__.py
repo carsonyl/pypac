dbfbf3658e46063be5c0445d92b7b50dab7ab273
Hiroki Fujii <hfujii@hisystron.com>
2020-11-15 17:38:20 +0900

    init関係修正

--- proxyUtil/__init__.py
+++ proxyUtil/__init__.py
@@ -19,11 +19,13 @@ limitations under the License.
 """
 
 from proxyUtil.api import get_pac, collect_pac_urls, download_pac, PACSession, pac_context_for_url, virtualProxyEnviron
+from . import proxyUtil
+
 
 
 __version__ = '0.1.1'
 
 
 __all__ = [
-    'get_pac', 'collect_pac_urls', 'download_pac', 'PACSession', 'pac_context_for_url', 'virtualProxyEnviron'
+    'get_pac', 'collect_pac_urls', 'download_pac', 'PACSession', 'pac_context_for_url', 'virtualProxyEnviron', 'proxyUtil'
 ]
